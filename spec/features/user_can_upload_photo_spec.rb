4e7aabd344cde08780e19918898ba41938ac1e73
Erinna Chen <erinna.chen@gmail.com>
2016-04-19 21:39:20 -0600

    Add test user can upload photo

---
+++ spec/features/user_can_upload_photo_spec.rb
@@ -0,0 +1,15 @@
+require "rails_helper"
+
+RSpec.feature "User can upload an image" do
+  include SpecHelpers
+  scenario "see page with image displayed" do
+    stub_omniauth
+    visit '/'
+    click_on "Sign in with Google"
+    click_on "PAW my puppy!"
+
+    expect(current_path).to eq "/uploads"
+    click_on "Add image"
+    click_on "Analyze image"
+  end
+end
