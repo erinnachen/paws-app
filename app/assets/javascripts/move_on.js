f363562409f201fddb12b822b60a92e9a084458c
Erinna Chen <erinna.chen@gmail.com>
2016-04-23 13:51:24 -0600

    Add image file to analyzer api request

--- app/assets/javascripts/move_on.js
+++ app/assets/javascripts/move_on.js
@@ -4,16 +4,22 @@ $( document ).ready(function() {
     console.log(pathname);
     var url = document.URL.split("/")
     var id = url[url.length - 1]
+    var image_url = $("#dog-image").attr("src")
 
-    $.getJSON("https://paws-api.herokuapp.com/api/v1/dog_image_categories",
+    //getJSON("http://162.243.13.94:8080/api/v1/dog_image_categories",
+    $.getJSON("http://localhost:5000/api/v1/dog_image_categories",
+      {image: image_url},
       function(result){
-        console.log(result["breed"])
-        console.log("Sending that post request")
+        console.log(result["breed"]);
+        console.log("Sending that post request");
         $.ajax({ url: "/dog_images/"+id,
                  type: 'PUT',
-                 data: "breed="+result["breed"]})
-        console.log('Load was performed.');
-        window.location.replace('/dog_images/'+id+'/analysis');
+                 data: "breed="+result["breed"],
+                 complete: function(data){
+                   console.log('Load was performed.');
+                   window.location.replace('/dog_images/'+id+'/analysis');
+                 }
+               })
       });
   }
 })
