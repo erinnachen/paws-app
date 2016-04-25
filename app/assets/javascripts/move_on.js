be84e4f43d0716fa450dda608c52fe9ca6f9d930
Erinna Chen <erinna.chen@gmail.com>
2016-04-25 14:35:36 -0600

    Add breeds to seed file
    * Restrict identification to breeds in the list

--- app/assets/javascripts/move_on.js
+++ app/assets/javascripts/move_on.js
@@ -1,7 +1,6 @@
 $( document ).ready(function() {
   if ($(".dog-image-show").length > 0) {
     var pathname = window.location.pathname;
-    console.log(pathname);
     var url = document.URL.split("/")
     var id = url[url.length - 1]
     var image_url = $("#dog-image").attr("src")
@@ -11,13 +10,10 @@ $( document ).ready(function() {
     $.getJSON("http://localhost:5000/api/v1/dog_image_categories",
       {image: image_url},
       function(result){
-        console.log(result["breed"]);
-        console.log("Sending that post request");
         $.ajax({ url: "/dog_images/"+id,
                  type: 'PUT',
-                 data: "breed="+result["breed"],
+                 data: "breed_id="+result["breed_id"],
                  complete: function(data){
-                   console.log('Load was performed.');
                    window.location.replace('/dog_images/'+id+'/analysis');
                  }
                })
